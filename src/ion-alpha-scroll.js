92c378d95075cdc5ede5059993b482e5ef0dcbc8
Deimantas <as.deimis@gmail.com>
2015-07-19 14:40:35 +0300

    Updated javascript
    Optimised plugin javascript code and added side alphabet auto sorting.

--- src/ion-alpha-scroll.js
+++ src/ion-alpha-scroll.js
@@ -1,98 +1,90 @@
 angular.module('ion-alpha-scroll', [])
 .directive('ionAlphaScroll', [
-	'$ionicScrollDelegate', '$location', '$timeout', '$document',
-	function ($ionicScrollDelegate, $location, $timeout, $document){
-		return{
-			require: '?ngModel',
-			restrict: 'AE',
-			replace: true,
-			compile: function(tElement, tAttrs, tTransclude){
-				var children = tElement.contents();
-				var template = angular.element([
-					'<ion-list class="ion_alpha_list_outer">',
-						'<ion-scroll delegate-handle="alphaScroll">',
-							'<div data-ng-repeat="(letter, items) in sorted_items" class="ion_alpha_list">',
-								'<ion-item class="item item-divider" id="index_{{letter}}">{{letter}}</ion-item>',
-								'<ion-item ng-repeat="item in items"></ion-item>',
-							'</div>',
-						'</ion-scroll>',
-						'<ul class="ion_alpha_sidebar">',
-							'<li ng-click="alphaScrollGoToList(\'index_{{letter}}\')" ng-repeat="letter in alphabet">{{ letter }}</li>',
-						'</ul>',
-					'</ion-list>'
-					].join(''));
+    '$ionicScrollDelegate', '$location', '$timeout', '$document',
+    function($ionicScrollDelegate, $location, $timeout, $document) {
+        return {
+            require: '?ngModel',
+            restrict: 'AE',
+            replace: true,
+            compile: function(tElement, tAttrs, tTransclude) {
+                var children = tElement.contents();
+                var template = angular.element([
+                    '<ion-list class="ion_alpha_list_outer">',
+                    '<ion-scroll delegate-handle="alphaScroll">',
+                    '<div data-ng-repeat="(letter, items) in sorted_items" class="ion_alpha_list">',
+                    '<ion-item class="item item-divider" id="index_{{letter}}">{{letter}}</ion-item>',
+                    '<ion-item ng-repeat="item in items"></ion-item>',
+                    '</div>',
+                    '</ion-scroll>',
+                    '<ul class="ion_alpha_sidebar">',
+                    '<li ng-click="alphaScrollGoToList(\'index_{{letter}}\')" ng-repeat="letter in alphabet | orderBy: letter">{{ letter }}</li>',
+                    '</ul>',
+                    '</ion-list>'
+                ].join(''));
 
-				var headerHeight = $document[0].body.querySelector('.bar-header').offsetHeight;
-				var subHeaderHeight = tAttrs.subheader === "true" ? 44 : 0;
-				var tabHeight = $document[0].body.querySelector('.tab-nav') ? $document[0].body.querySelector('.tab-nav').offsetHeight : 0;
-				var windowHeight = window.innerHeight;
+                var headerHeight = $document[0].body.querySelector('.bar-header').offsetHeight;
+                var subHeaderHeight = tAttrs.subheader === "true" ? 44 : 0;
+                var tabHeight = $document[0].body.querySelector('.tab-nav') ? $document[0].body.querySelector('.tab-nav').offsetHeight : 0;
+                var windowHeight = window.innerHeight;
 
-				var contentHeight = windowHeight - headerHeight - subHeaderHeight - tabHeight;
+                var contentHeight = windowHeight - headerHeight - subHeaderHeight - tabHeight;
 
-				angular.element(template.find('ion-item')[1]).append(children);
-				tElement.html('');
-      			tElement.append(template);
+                angular.element(template.find('ion-item')[1]).append(children);
+                tElement.html('');
+                tElement.append(template);
 
-      			tElement.find('ion-scroll').css({"height": contentHeight + 'px'});
+                tElement.find('ion-scroll').css({
+                    "height": contentHeight + 'px'
+                });
 
-				return function (scope, element, attrs, ngModel) {
-					var count = 0;
-					var scrollContainer = element.find('ion-scroll');
+                return function(scope, element, attrs, ngModel) {
+                    var count = 0;
+                    var scrollContainer = element.find('ion-scroll');
 
-					var ionicScroll = scrollContainer.controller('$ionicScroll');
+                    var ionicScroll = scrollContainer.controller('$ionicScroll');
 
-	                // do nothing if the model is not set
-	                if (!ngModel) return;
+                    // do nothing if the model is not set
+                    if (!ngModel) return;
 
-	                ngModel.$render = function(){
-						scope.items = [];
-	                	scope.items = ngModel.$viewValue;
-		                var tmp={};
-			            for (i=0;i<scope.items.length;i++){
-		              		var letter = scope.items[i][attrs.key].toUpperCase().charAt(0);
-			              	if (tmp[letter] == undefined){
-			        			tmp[letter]=[]
-				            }
-			              	tmp[letter].push(scope.items[i]);
-			            }
-	                	scope.alphabet = iterateAlphabet(tmp);
-			            scope.sorted_items = tmp;
+                    ngModel.$render = function() {
+                        scope.items = [];
+                        scope.items = ngModel.$viewValue;
+                        var tmp = {};
+                        for (i = 0; i < scope.items.length; i++) {
+                            var letter = scope.items[i][attrs.key].toUpperCase().charAt(0);
+                            if (tmp[letter] == undefined) {
+                                tmp[letter] = []
+                            }
+                            tmp[letter].push(scope.items[i]);
+                        }
+                        scope.alphabet = iterateAlphabet(tmp);
+                        scope.sorted_items = tmp;
 
-			            scope.alphaScrollGoToList = function(id){
-				          $location.hash(id);
-				          $ionicScrollDelegate.$getByHandle('alphaScroll').anchorScroll();
-				        }
+                        scope.alphaScrollGoToList = function(id) {
+                            $location.hash(id);
+                            $ionicScrollDelegate.$getByHandle('alphaScroll').anchorScroll();
+                        }
 
-				        //Create alphabet object
-				        function iterateAlphabet(alphabet){
-				           var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-							if (Object.keys(alphabet).length != 0){
-								str = '';
-								for (var i=0; i<Object.keys(alphabet).length; i++){
-									str += Object.keys(alphabet)[i];
-								}
-							}
-				           var numbers = new Array();
-				           for (var i=0; i<str.length; i++){
-				              var nextChar = str.charAt(i);
-				              numbers.push(nextChar);
-				           }
-				           return numbers;
-				        }
-				        scope.groups = [];
-				        for (var i=0; i<10; i++) {
-				          scope.groups[i] = {
-				            name: i,
-				            items: []
-				          };
-				          for (var j=0; j<3; j++) {
-				            scope.groups[i].items.push(i + '-' + j);
-				          }
-				        }
+                        //Create alphabet object
+                        function iterateAlphabet(alphabet) {
+                            var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                            if (Object.keys(alphabet).length != 0) {
+                                str = '';
+                                for (var i = 0; i < Object.keys(alphabet).length; i++) {
+                                    str += Object.keys(alphabet)[i];
+                                }
+                            }
+                            var numbers = new Array();
+                            for (var i = 0; i < str.length; i++) {
+                                var nextChar = str.charAt(i);
+                                numbers.push(nextChar);
+                            }
+                            return numbers;
+                        }
 
-	                };
-
-	            }
-	        }
+                    };
+                }
+            }
         };
-    }]);
+    }
+]);
